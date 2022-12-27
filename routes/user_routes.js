bd7b258128446f5bf87e791e73f2947ecffbf0ef
Samyog Bohara <fallendemon174@gmail.com>
2022-12-27 22:24:57 +0545

    login

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 const User = require('../Model/User')
 
 const router = express.Router()
@@ -26,6 +27,38 @@ router.post('/register',(req, res, next) => {
                         role: user.role
                     })
                 }).catch(next)
+            })
+        }).catch(next)
+})
+
+router.post('/login', (req,res,next) => {
+    User.findOne({username: req.body.username})
+        .then(user => {
+            if(user==null){
+                let err = new Error(`User ${req.body.username} has not been registered`)
+                req.status(404)
+                return next(err)
+            }
+            bcrypt.compare(req.body.password, user.password, (err, status) => {
+                if(err) return next(err)
+                if(!status) {
+                    let err = new Error('Password does not match')
+                    res.status(401)
+                    return next(err)
+                }
+                let data = {
+                    userId: user._id,
+                    username: user.username,
+                    role: user.role
+                }
+                jwt.sign(data, process.env.SECRET,
+                    {'expiresIn': '1d'}, (err, token) => {
+                        if(err) return next(err)
+                        res.json({
+                            'status': 'User was logged in successfully',
+                            token: token
+                        })
+                    })
             })
         }).catch(next)
 })
