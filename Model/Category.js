771e18d57d85c59af4a2d433c3aae11d0e6a361c
Samyog Bohara <fallendemon174@gmail.com>
2022-12-29 18:02:13 +0545

    category schema

---
+++ Model/Category.js
@@ -0,0 +1,14 @@
+const mongoose = reqquire('mongoose')
+
+const categorySchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Category should be specified']
+    },
+    blogs: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Blog'
+    }]
+})
+
+module.exports = mongoose.model('Category', categorySchema)
