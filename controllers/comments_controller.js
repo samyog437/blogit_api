008b224f58bcf463340bb5f6281e4da753c72cfb
Samyog Bohara <fallendemon174@gmail.com>
2023-01-13 17:16:45 +0545

    create comment

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -6,3 +6,21 @@ const getAllComments = (req, res, next) => {
         res.json(blog.comments)
     }).catch(next)
 }
+
+const createComment = (req, res, next) => {
+    Blog.findById(req.params.id)
+    .then((blog) => {
+        let comment = {
+            "body": req.body.body,
+            "commenter_id": req.user.userId
+        }
+        blog.comments.push(comment)
+        blog.save()
+            .then((b) => res.status(201).json(b.comments))
+    }).catch(next)
+}
+
+module.exports = {
+    getAllComments,
+    createComment,
+}
