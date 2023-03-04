63a1291b2927e15fc679a1b97303ba7b4c08dae0
Samyog Bohara <fallendemon174@gmail.com>
2023-03-04 18:57:50 +0545

    test

--- tests/user.test.js
+++ tests/user.test.js
@@ -5,9 +5,20 @@ const mongoose = require('mongoose')
 
 const api = supertest(app)
 
-const user = {
-    username: "testuser1",
+const loginuser = {
+    username: "testuser3",
     password: "password"
+}
+
+const registeruser = {
+    username: "testuser3",
+    password:"password",
+    email:"testuser3@email.com"
+}
+
+const blog = {
+    title: "This is title 1",
+    content: "This is content 1"
 }
 
 beforeAll(async()=> {
@@ -16,11 +27,27 @@ beforeAll(async()=> {
 
 test('User Registration', async() => {
     await api.post('/user/')
-    .send(user)
+    .send(registeruser)
     .expect(201)
     .expect(res => {
         expect(res.body.status).toContain('User has registered successfully')
     })
+})
+
+test('User Login', async() => {
+    await api.post('/user/login')
+    .send(loginuser)
+    .expect(200)
+    .expect(res => {
+        expect(res.body.status).toContain('User was logged in successfully')
+    })
+
+})
+
+test('Blog create', async() => {
+    await api.post('/blogs')
+    .send(blog)
+    .expect(201)
 })
 
 afterAll(async() => {
